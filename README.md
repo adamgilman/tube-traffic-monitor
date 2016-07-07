a1c789d7aa9eb9e87e1e036df37b4532cc91c942
Adam Gilman <adam.gilman@gmail.com>
2016-07-07 16:57:58 +0100

    blank slate

--- README.md
+++
@@ -1,2 +0,0 @@
-# tube-traffic-monitor
-Monitor the current traffic patterns occurring the London Underground (Tube)
--- tubetrafficmonitor/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Adam Gilman'
-__email__ = 'me@adamgilman.com'
-__version__ = '.01a'
-
-
-from TTM import TTM
